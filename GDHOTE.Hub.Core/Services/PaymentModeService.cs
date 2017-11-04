18ef94ebf5faa236921c230f8ca5ccb8e6f2e9eb
Olufunso <abrabfun@yahoo.com>
2017-11-04 23:19:23 +0100

    Paymode mode service commited

--- GDHOTE.Hub.Core/Services/PaymentModeService.cs
+++ GDHOTE.Hub.Core/Services/PaymentModeService.cs
@@ -15,7 +15,8 @@ namespace GDHOTE.Hub.Core.Services
             {
                 new PaymentMode {Id = 1, Code = "C", Description = "Cash"},
                 new PaymentMode {Id = 2, Code = "T", Description = "Bank Transfer"},
-                new PaymentMode {Id = 3, Code = "P", Description = "POS"}
+                new PaymentMode {Id = 3, Code = "P", Description = "POS"},
+                new PaymentMode {Id = 4, Code = "W", Description = "Web"}
             };
             return paymentModes;
         }
