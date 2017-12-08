1bc2d8eb3c550f6c9559393274a4d3348a6b2041
Olufunso <abrabfun@yahoo.com>
2017-12-08 23:32:08 +0100

    Sub Menu View Logic started

---
+++ GDHOTE.Hub.Core/Models/SubMenuView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+namespace GDHOTE.Hub.Core.Models
+{
+    [TableName("vx_HUB_SubMenus")]
+    public class SubMenuView : SubMenu
+    {
+
+    }
+}
