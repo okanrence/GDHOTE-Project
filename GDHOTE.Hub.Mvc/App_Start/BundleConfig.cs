05f0e27b24c8c3a386583882aba50f829efefd5b
Olanrewaju <okanrence@gmail.com>
2018-10-26 00:16:22 +0100

    added admin lte

--- GDHOTE.Hub.Mvc/App_Start/BundleConfig.cs
+++ GDHOTE.Hub.Mvc/App_Start/BundleConfig.cs
@@ -10,18 +10,28 @@ namespace GDHOTE.Hub.Mvc
         {
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/bootbox.js",
-                        "~/Scripts/respond.js",
-                        "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js",
-                        "~/Scripts/moment.js",
-                       "~/Scripts/bootstrap-datepicker.js",
-                       "~/Scripts/sweetalert.min.js",
-                       "~/Scripts/jquery.unobtrusive-ajax.js",
-                       "~/Scripts/typeahead.bundle.js",
-                       "~/Scripts/select2.full.js",
-                        "~/Scripts/jquery-ui-{version}.js"
+                        "~/Content/bower_components/bootstrap/dist/js/bootstrap.min.js",
+                        //"~/Content/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js",
+                        //"~/Content/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js",
+                        //"~/Content/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+                        //"~/Content/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+                        //"~/Content/bower_components/moment/min/moment.min.js",
+                        //"~/Content/bower_components/bootstrap-daterangepicker/daterangepicker.js",
+                        //"~/Content/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js",
+                        //"~/Content/bower_components/jquery-slimscroll/jquery.slimscroll.min.js",
+                        "~/Content/dist/js/adminlte.min.js"
+                        //"~/Content/dist/js/pages/dashboard.js",
+                        ////"~/Scripts/bootbox.js",
+                        //"~/Scripts/respond.js",
+                        //"~/Scripts/datatables/jquery.datatables.js",
+                        //"~/Scripts/datatables/datatables.bootstrap.js",
+                        //"~/Scripts/moment.js",
+                        //"~/Scripts/bootstrap-datepicker.js",
+                        //"~/Scripts/sweetalert.min.js",
+                        //"~/Scripts/jquery.unobtrusive-ajax.js",
+                        //"~/Scripts/typeahead.bundle.js",
+                        //"~/Scripts/select2.full.js",
+                        //"~/Scripts/jquery-ui-{version}.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -35,14 +45,23 @@ namespace GDHOTE.Hub.Mvc
 
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/bower_components/font-awesome/css/font-awesome.min.css",
+                      "~/Content/bower_components/Ionicons/css/ionicons.min.css",
+                      //"~/Content/bower_components/morris.js/morris.css",
+                      //"~/Content/bower_components/jvectormap/jquery-jvectormap.css",
+                      //"~/Content/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
+                      //"~/Content/bower_components/bootstrap-daterangepicker/daterangepicker.css",
+                      //"~/Content/bower_components/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
-                      "~/Content/bootstrap-datepicker.css",
-                      "~/Content/sweetalert.min.css",
+                     "~/Content/sweetalert.min.css",
                       "~/Content/typeahead.css",
                       "~/Content/select2.css",
-                      "~/Content/jquery-ui/jquery-ui.min.css",
-                      "~/Content/site.css")); 
+                       "~/Content/jquery-ui/jquery-ui.min.css",
+                       "~/Content/dist/css/AdminLTE.min.css",
+                      "~/Content/dist/css/skins/skin-blue.min.css"
+                     
+                      /*"~/Content/site.css"*/)); 
         }
     }
 }
